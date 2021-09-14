b3176cac2f998bab14908b5da7915b68fa54fd88
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 23:26:51 -0400

    Commit

--- src/main/java/exercise22.java
+++ src/main/java/exercise22.java
@@ -8,5 +8,15 @@ import java.util.Scanner;
 public class exercise22 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int first = input.nextInt();
+        System.out.println("Enter the second number:");
+        int second = input.nextInt();
+        System.out.println("Enter the third number:");
+        int third = input.nextInt();
+        int max;
+        max = (first > second) ? first : second;
+        max = (third > max) ? third : max;
+        System.out.printf("The largest number is %d.", max);
     }
 }
